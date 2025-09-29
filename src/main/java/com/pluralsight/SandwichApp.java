8b71e50ac32242d3ea9c787fa748d4d5d48758cb
David-Aldair <d.aldair.quinonez@gmail.com>
2025-09-29 12:01:02 -0400

    2nd Commit

--- src/main/java/com/pluralsight/SandwichApp.java
+++ src/main/java/com/pluralsight/SandwichApp.java
@@ -1,8 +1,63 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class SandwichApp {
     public static void main(String[] args) {
 
+        Scanner I = new Scanner(System.in);
+
+        System.out.println("""
+                            Welcome to our wonderful Sandwich Shop!
+                            Pick a Sandwich
+                            (1) Regular: $5.45
+                            (2) Large: $8.95
+                            """);
+
+        int size = I.nextInt();
+
+        double price = 0;
+
+        if (size == 1){
+            price = 5.45;
+        }
+        else if (size == 2){
+            price = 8.95;
+        }
+
+        System.out.println("What is your age: ");
+
+        int age = I.nextInt();
+
+        if (age >= 65) {
+
+            price *= 0.8;
+        }
+
+        else if (age <= 17){
+
+            price *= 0.9;
+
+        }
+
+        System.out.println("The final price will be: $" + price);
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
 
     }
-}
+
